5e630b0af12b9971d08fcd5b2cf22a8433eb3b6d
pavelchlebek <pavelchlebek990@gmail.com>
2022-05-09 08:26:35 +0200

    first commit after cra

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import data from './data/data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.items[0].pictures.map((item, index) => {
+        return <img style={{ width: "150px", height: "150px" }} key={index} src={item} alt="img" />
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
